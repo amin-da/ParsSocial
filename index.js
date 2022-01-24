0f39cdfa1ad0fc493c59a6cc4929dac0137f26ec
amin-da <darabniya@yahoo.com>
2022-01-24 11:21:46 +0330

    compelete functionality in js file for notification and messages in sidebar

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-// sidebar in responsive mode
+// Functionality for open and close sidebar in responsive mode
 var btnSidebarClose = document.getElementById("btn-sidebar-close");
 var btnSidebarOpen = document.getElementById("btn-sidebar-open");
 var sidebar = document.getElementById("sidebar");
+
+var messageNotification = document.querySelector("#mesgges-notification");
+var messages = document.querySelector("#messages");
 
 btnSidebarClose.addEventListener("click", () => {
   sidebar.classList.add("close-menu");
@@ -9,4 +12,43 @@ btnSidebarClose.addEventListener("click", () => {
 
 btnSidebarOpen.addEventListener("click", () => {
   sidebar.classList.remove("close-menu");
+});
+
+// Functionality for active each item in sidebar when clicked !
+const menuItems = document.querySelectorAll(".menu-item");
+
+// Remove active class in other Items
+changeActiveClass = () => {
+  menuItems.forEach(item => {
+    if (item.classList.contains("active")) {
+      item.classList.remove("active");
+    } else if (messages.getAttribute("style").includes("boxShadow") != null) {
+      messages.style.boxShadow = "none";
+    }
+  });
+};
+
+// Add active class
+menuItems.forEach(item => {
+  item.addEventListener("click", () => {
+    changeActiveClass();
+    item.classList.add("active");
+    if (item.id != "notifications") {
+      document.querySelector(".notifications-popup").style.display = "none";
+    } else {
+      document.querySelector(".notifications-popup").style.display = "block";
+      document.querySelector(
+        "#notifications .notification-count"
+      ).style.display = "none";
+    }
+  });
+});
+
+// Functionality for Messages
+
+messageNotification.addEventListener("click", () => {
+  messages.style.boxShadow = "0 0 1rem var(--color-primary)";
+  messageNotification.querySelector(".notification-count").style.display =
+    "none";
+  // setTimeout(() => {}, 2000);
 });
